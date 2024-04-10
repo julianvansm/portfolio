e00a09d79629eec8f65a0714f658e45a3be78a81
julian van smirren <julianvansm@gmail.com>
2024-04-10 09:12:46 +0200

    online

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import HelloWorld from './components/Welcome.vue'
+import Project from "./components/Projects/Project.vue";
 
 </script>
 
 <template>
 
-  <HelloWorld />
+  <router-view> </router-view>
+
 </template>
 
 <style scoped>
